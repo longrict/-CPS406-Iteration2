75de153a39564bd25465d6d2d30fab1feb79da18
vcarnovale <122923089+vcarnovale@users.noreply.github.com>
2024-04-10 14:47:50 -0400

    Added client side functions for register authorization

--- bugReportApplication/src/main/java/bugreportapplication/authorization/registerAuthorization.java
+++ bugReportApplication/src/main/java/bugreportapplication/authorization/registerAuthorization.java
@@ -9,34 +9,54 @@ package bugreportapplication.authorization;
  * @author vito
  */
 public class registerAuthorization {
-    // Checks if the email given is already associated with another user
-    public static boolean checkEmailExistence(String email){
-        return true;
-    }
     
     //OPTIONAL --> Checks if email is of the form user@emailservice.xxx
     public static boolean checkEmailCorrectness(String email){
         return true;
     }
     
-    // Checks if the password is the correct length (8), has a capital letter
+    // Checks if the password is the correct length (8) or larger, has a capital letter
     public static boolean checkPassword(char[] password){
-        return true;
+        int passSize = password.length;
+        boolean correct = false;
+        if (passSize >= 8){
+            for (int i = 0; i < passSize; i++){
+                if (Character.isUpperCase(password[i])){
+                    correct = true;
+                }
+                // For security
+                password[i] = 0;
+            }
+        }
+        return correct;
     }
+    
     
     // Verifies password and verifypassword are the same
-    public static boolean checkVerify(char[] password){
-        return true;
-    }
-    
-    
-    // Checks if username is already associated with an existing user
-    public static boolean checkUserExistence(String user){
+    public static boolean checkVerify(char[] password, char[] verify){
+        int passLen = password.length;
+        if (passLen == verify.length){
+            for (int i = 0; i < passLen; i++){
+                if (password[i] != verify[i]){
+                    return false;
+                }
+            }
+        }else {
+            return false;
+        }
+        
         return true;
     }
     
     // Checks if a username is the correct length 6 characters and is alphanumeric
     public static boolean checkUserCorrectness(String user){
+        if (user.length() > 6){
+            for (int i = 0; i < user.length(); i++){
+                if (!(Character.isAlphabetic(user.charAt(i)) || Character.isDigit(user.charAt(i)))){
+                    return false;
+                }
+            }
+        }
         return true;
     }
     
