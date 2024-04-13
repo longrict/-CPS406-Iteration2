050672a16df78fa3cbe23e5df5d46d973247ef35
longrict <125839240+longrict@users.noreply.github.com>
2024-04-12 21:51:49 -0400

    comments for SendEmailTLS.java

--- bugReportApplication/src/main/java/bugreportapplication/SendEmailTLS.java
+++ bugReportApplication/src/main/java/bugreportapplication/SendEmailTLS.java
@@ -5,10 +5,25 @@ import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 import java.util.Properties;
 
+/**
+ *
+ * @author David Liu, Longric Tran, Refath Ahmed, Tao Wang, Vito Carnovale (Group #33)
+ *
+ * Represents a SendEmailsTLS class for functions related to the email notification system
+ */
 public class SendEmailTLS {
 
-
+    /**
+     *
+     * sends an email to a user
+     *
+     * @param email          the email address that the notification email will be sent to
+     * @param subjectLine    the subject line for the email that is to be sent
+     * @param emailMessage   the body of the email that is to be sent
+     */
     public static void send(String email,String subjectLine,String emailMessage) {
+
+        // account details for the account that sends the email
         final String username = "cps420group33@gmail.com";
         final String password = null;
 
@@ -18,6 +33,8 @@ public class SendEmailTLS {
         prop.put("mail.smtp.auth", "true");
         prop.put("mail.smtp.starttls.enable", "true"); //TLS
 
+
+        // authenticate login for the account that sends the mail
         Session session = Session.getInstance(prop,
                 new javax.mail.Authenticator() {
                     protected PasswordAuthentication getPasswordAuthentication() {
@@ -26,7 +43,7 @@ public class SendEmailTLS {
                 });
 
         try {
-
+        // try to send the email
             Message message = new MimeMessage(session);
             message.setFrom(new InternetAddress("cps420group33@gmail.com"));
             message.setRecipients(
